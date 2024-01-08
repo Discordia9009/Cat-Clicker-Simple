266c4b9e884ec78caf69b1173151692bd4da0c90
Discordia9009 <cpraouvir@gmail.com>
2024-01-08 23:11:36 +0000

    MaisGatoClicavel

--- CookieClicar/Button3Script.gd
+++ CookieClicar/Button3Script.gd
@@ -8,6 +8,9 @@ func _pressed():
 	if label.score >= 40:
 		audio_stream_player_2d.play()
 		label.score -= 40
-		gatin.texture = load("res://gatingatindeoiverde.png")
+		if gatin.texture == load("res://gatin.png"):
+			gatin.texture = load("res://gatingatindeoiverde.png")
+		elif gatin.texture == load("res://gatincomchapeu.png"):
+			gatin.texture = load("res://gatingatindeoiverdecomchapeu.png")
 		sprite_2d_3.texture = load("res://CadeadoAberto.png")
 		self.disabled = true
---
+++ CookieClicar/Button4Script.gd
@@ -0,0 +1,21 @@
+extends Button
+@onready var gatin = $"../../../Area2D/Gatin"
+@onready var sprite_2d_2 = $Sprite2D2
+@onready var audio_stream_player_2d = $"../../../AudioStreamPlayer2D"
+@onready var label = $"../../../Label"
+
+func _pressed():
+	audio_stream_player_2d.play()
+	if label.score >= 50:
+		label.score -= 50
+		if gatin.texture == load("res://gatin.png"):
+			 
+			gatin.texture = load("res://gatincomchapeu.png")
+			gatin.position = Vector2(-0.551, -1.702)
+			self.disabled = true
+			sprite_2d_2.texture = load("res://CadeadoAberto.png")
+		elif gatin.texture == load("res://gatingatindeoiverde.png"):
+			gatin.texture = load("res://gatingatindeoiverdecomchapeu.png")
+			gatin.position = Vector2(-0.551, -1.702)
+			self.disabled = true
+			sprite_2d_2.texture = load("res://CadeadoAberto.png")
